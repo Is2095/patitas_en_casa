cea41999f25a8a7f3c03c32353ed47b6c0530557
unknown <ismael2095951@gmail.com>
2023-10-09 23:23:33 -0300

    conección con base de datos local mongodb

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,14 @@
 
 import "dotenv/config";
 import app from './src/server/server';
+import { conectar } from "./src/baseDeDatos/conectar";
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+
+
+
 
 app.listen(PORT, () => {
+    conectar();
     console.log(`Server inicializado en el puerto ${PORT}`);  
-})
+});
---
+++ server/src/baseDeDatos/conectar.ts
@@ -0,0 +1,26 @@
+
+import mongoose from "mongoose";
+//import "dotenv/config";
+
+const { MONGODB_URL } = process.env;
+
+if (!MONGODB_URL) {
+    throw new Error('URL de conección no existe')
+}
+
+export const conectar = async (): Promise<boolean> => {
+
+    try {
+        const { connection } = await mongoose.connect(MONGODB_URL as string);
+
+        if (connection.readyState === 1) {
+            console.log('Mongodb conectada');
+            return Promise.resolve(true);
+        }
+        return Promise.reject(false)
+    } catch (error) {
+        console.log(error);
+        return Promise.reject(false)
+    }
+
+};
